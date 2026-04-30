591f3d9dd399d9a7d50ef70981afcc8742c4e217
AdamNandor <adamn@kkszki.hu>
2026-04-30 11:28:12 +0200

    kinevetavegen alap

--- JS/kinevetavegen.js
+++ JS/kinevetavegen.js
@@ -1,3 +1,9 @@
 document.getElementById("VisszaButton").onclick = function(){
     window.location.href = "fooldal.html";
 }
+let vNegyzet = document.getElementById("VorosNegyzet");
+vNegyzet.onclick = function(){
+    vNegyzet.src = "IMG/kiNevetAVegen/zöld-négyzet.png";
+    vNegyzet.alt = "zold negyzet";
+    vNegyzet.title = "zold negyzet";
+}
