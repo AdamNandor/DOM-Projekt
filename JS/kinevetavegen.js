46100b7bfef576aff4400739a3aad5a690718596
AdamNandor <adamn@kkszki.hu>
2026-05-10 03:17:28 +0200

    kinevetavegen mukodik de meg design kell

--- JS/kinevetavegen.js
+++ JS/kinevetavegen.js
@@ -1,9 +1,31 @@
 document.getElementById("VisszaButton").onclick = function(){
     window.location.href = "fooldal.html";
 }
+
 let vNegyzet = document.getElementById("VorosNegyzet");
+let indulasIdo = null;
+
 vNegyzet.onclick = function(){
+    if (indulasIdo !== null) 
+    {
+        let reakcioIdo = Date.now() - indulasIdo;
+        indulasIdo = null;
+        alert("Reakcióidő: " + reakcioIdo + "ms");
+
+        vNegyzet.src = "IMG/piros-negyzet.png";
+        vNegyzet.alt = "piros negyzet";
+        vNegyzet.title = "piros negyzet";
+    }
+    else 
+    {
+        let rnd = Math.floor(Math.random() * 4000) + 1000;
+        setTimeout(Zoldre, rnd);
+    }
+}
+
+function Zoldre() {
     vNegyzet.src = "IMG/kiNevetAVegen/zöld-négyzet.png";
     vNegyzet.alt = "zold negyzet";
     vNegyzet.title = "zold negyzet";
+    indulasIdo = Date.now();
 }
